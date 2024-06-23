a320f948dd8d6668de4edd48aee15ec159075bfc
Mark <41304553+MarkV2323@users.noreply.github.com>
2024-06-23 12:16:57 -0600

    Added Main, Collision component & CMake to build library

---
+++ src/Main.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include <SFML/Graphics.hpp>
+
+using namespace std;
+
+/**
+ * This Main program is used simply as a quick test bench for changes 
+ * in the ECS-Engine library.
+ */
+int main(int argc, char const *argv[])
+{
+    cout << "Hello World!\n";
+    return 0;
+}
